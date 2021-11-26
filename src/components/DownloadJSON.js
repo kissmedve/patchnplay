a14bee5b1715db01d8f0b74d9a747496a1d1b0d7
Ursula Gottschall <ursula.gottschall@webberie.de>
2021-11-26 13:26:05 +0100

    amended DownloadJSON and UploadJSON to reflect the addition of FabricsContext

--- src/components/DownloadJSON.js
+++ src/components/DownloadJSON.js
@@ -2,13 +2,29 @@ import React, { useContext } from "react";
 import { SquaresContext } from "./SquaresContext";
 import { BigBlocksContext } from "./BigBlocksContext";
 import { ColorsContext } from "./ColorsContext";
+import { FabricsContext } from "./FabricsContext";
 
 const DownloadJSON = () => {
-
   // global states
-  const { cols, rows, sashingCols, sashingRows, sashingWidths, sashingHeights, sashingColsColor, sashingRowsColor, squares, insertedBigBlocks, borders, squareWidth, borderBaseWidth } = useContext(SquaresContext);
+  const {
+    cols,
+    rows,
+    sashingCols,
+    sashingRows,
+    sashingWidths,
+    sashingHeights,
+    sashingColsColor,
+    sashingRowsColor,
+    squares,
+    insertedBigBlocks,
+    borders,
+    squareWidth,
+    borderBaseWidth,
+  } = useContext(SquaresContext);
   const { selectedBigBlocks } = useContext(BigBlocksContext);
   const { paletteColors } = useContext(ColorsContext);
+  const { fabricWidths, fabricSquareWidth, seamAllowance } =
+    useContext(FabricsContext);
 
   const data = {
     cols,
@@ -26,10 +42,14 @@ const DownloadJSON = () => {
     borderBaseWidth,
     selectedBigBlocks,
     paletteColors,
+    fabricWidths,
+    fabricSquareWidth,
+    seamAllowance,
   };
 
   return (
-    <a className="btn btn-link"
+    <a
+      className="btn btn-link"
       type="button"
       href={`data:text/json;charset=utf-8,${encodeURIComponent(
         JSON.stringify(data)
@@ -39,9 +59,7 @@ const DownloadJSON = () => {
       <img src="icon-download.svg" alt="Download" />
       <span>Download</span>
     </a>
-  )
-
-}
+  );
+};
 
 export default DownloadJSON;
-
--- src/components/UploadJSON.js
+++ src/components/UploadJSON.js
@@ -2,74 +2,89 @@ import React, { useContext, useState } from "react";
 import { SquaresContext } from "./SquaresContext";
 import { BigBlocksContext } from "./BigBlocksContext";
 import { ColorsContext } from "./ColorsContext";
+import { FabricsContext } from "./FabricsContext";
 import Message from "./Message";
 
 const UploadJSON = () => {
-
   // global states
-  const { updateSquares, updateCols, updateRows, updateSashingCols, updateSashingRows, updateSashingWidths, updateSashingHeights, updateSashingColsColor, updateSashingRowsColor, updateInsertedBigBlocks, updateBorders } = useContext(SquaresContext);
+  const {
+    updateSquares,
+    updateCols,
+    updateRows,
+    updateSashingCols,
+    updateSashingRows,
+    updateSashingWidths,
+    updateSashingHeights,
+    updateSashingColsColor,
+    updateSashingRowsColor,
+    updateInsertedBigBlocks,
+    updateBorders,
+  } = useContext(SquaresContext);
   const { updateBigBlocks } = useContext(BigBlocksContext);
   const { updateColors } = useContext(ColorsContext);
+  const { updateFabricWidths, updateFabricSquareWidth, updateSeamAllowance } =
+    useContext(FabricsContext);
 
   // local state
-  const [selectedFile, setSelectedFile] = useState('');
+  const [selectedFile, setSelectedFile] = useState("");
   const [messageIsActive, setMessageIsActive] = useState(false);
-  const [messageText, setMessageText] = useState('');
+  const [messageText, setMessageText] = useState("");
 
   const onFileChange = (event) => {
-
     let file = event.target.files[0];
     if (file.type === "application/json") {
       setSelectedFile(file);
     } else {
-      setMessageText('File type not allowed');
+      setMessageText("File type not allowed");
       setMessageIsActive(true);
     }
-  }
+  };
 
   const onFileUpload = async (event) => {
-
     event.preventDefault();
 
-    if (selectedFile !== '') {
-    const fileData = await selectedFile.text();
-    let parsedData = JSON.parse(fileData);
+    if (selectedFile !== "") {
+      const fileData = await selectedFile.text();
+      let parsedData = JSON.parse(fileData);
 
-    updateSquares(parsedData.squares);
-    updateCols(parsedData.cols);
-    updateRows(parsedData.rows);
-    updateSashingCols(parsedData.sashingCols);
-    updateSashingRows(parsedData.sashingRows);
-    updateSashingWidths(parsedData.sashingWidths);
-    updateSashingHeights(parsedData.sashingHeights);
-    updateSashingColsColor(parsedData.sashingColsColor);
-    updateSashingRowsColor(parsedData.sashingRowsColor);
-    updateInsertedBigBlocks(parsedData.insertedBigBlocks);
-    updateBorders(parsedData.borders);
+      updateSquares(parsedData.squares);
+      updateCols(parsedData.cols);
+      updateRows(parsedData.rows);
+      updateSashingCols(parsedData.sashingCols);
+      updateSashingRows(parsedData.sashingRows);
+      updateSashingWidths(parsedData.sashingWidths);
+      updateSashingHeights(parsedData.sashingHeights);
+      updateSashingColsColor(parsedData.sashingColsColor);
+      updateSashingRowsColor(parsedData.sashingRowsColor);
+      updateInsertedBigBlocks(parsedData.insertedBigBlocks);
+      updateBorders(parsedData.borders);
 
-    updateBigBlocks(parsedData.selectedBigBlocks);
+      updateBigBlocks(parsedData.selectedBigBlocks);
 
-    updateColors(parsedData.paletteColors);
-  } else {
-    setMessageText('Please choose a file');
-    setMessageIsActive(true);
-  }
-} 
+      updateColors(parsedData.paletteColors);
 
-const closeMessage = (event) => {
-  setMessageIsActive(false);
-  setMessageText('');
-}
+      updateFabricWidths(parsedData.fabricWidths);
+      updateFabricSquareWidth(parsedData.fabricSquareWidth);
+      updateSeamAllowance(parsedData.seamAllowance);
+    } else {
+      setMessageText("Please choose a file");
+      setMessageIsActive(true);
+    }
+  };
+
+  const closeMessage = (event) => {
+    setMessageIsActive(false);
+    setMessageText("");
+  };
 
   return (
     <div className="card">
-
       <div className="card-body">
-
         <form onSubmit={onFileUpload}>
           <div className="form-group">
-            <label className="form-label">Choose File
-            <input
+            <label className="form-label">
+              Choose File
+              <input
                 className="form-input file-input"
                 type="file"
                 name="fileUpload"
@@ -78,18 +93,18 @@ const closeMessage = (event) => {
               ></input>
             </label>
           </div>
-          {messageIsActive ?
-              <Message text={messageText} closeMessage={closeMessage} />
-              : ''}
-          <button className="btn" type="submit" name="save" >
+          {messageIsActive ? (
+            <Message text={messageText} closeMessage={closeMessage} />
+          ) : (
+            ""
+          )}
+          <button className="btn" type="submit" name="save">
             Upload
-            </button>
+          </button>
         </form>
-
       </div>
     </div>
-  )
-
-}
+  );
+};
 
 export default UploadJSON;
