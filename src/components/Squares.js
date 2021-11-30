3491e1b9dfdd766dd7cf26bab3fdd8c101ae5ef9
Ursula Gottschall <ursula.gottschall@webberie.de>
2021-11-30 19:45:13 +0100

    made the layout centered, improved coloring

--- src/components/Squares.js
+++ src/components/Squares.js
@@ -13,149 +13,205 @@ import Border from "./Border";
 import BorderStyler from "./BorderStyler";
 
 const Squares = () => {
-
   // global states
-  const { squares, cols, sashingWidths, sashingHeights, insertedBigBlocks, squareWidth, borders, borderBaseWidth } = useContext(SquaresContext);
-  const { sashStylerIsOpen, activeSashStyler, openSashStyler, squStylerIsOpen, bigBlockStylerIsOpen, activeBigBlockStyler, openBigBlockStyler, reopenBigBlockStyler, openCalcStyler, openBorderStyler, calcStylerIsOpen, borderStylerIsOpen } = useContext(StylersContext);
+  const {
+    squares,
+    cols,
+    sashingWidths,
+    sashingHeights,
+    insertedBigBlocks,
+    squareWidth,
+    borders,
+    borderBaseWidth,
+  } = useContext(SquaresContext);
+  const {
+    sashStylerIsOpen,
+    activeSashStyler,
+    openSashStyler,
+    reopenBigBlockStyler,
+    openBorderStyler,
+    borderStylerIsOpen,
+  } = useContext(StylersContext);
 
   // summed up widths of borders
-  let bordersTopWidth = borders.map(border => border.widthTop).reduce((acc, curr) => acc + curr);
-  let bordersRightWidth = borders.map(border => border.widthRight).reduce((acc, curr) => acc + curr);
-  let bordersBottomWidth = borders.map(border => border.widthBottom).reduce((acc, curr) => acc + curr);
-  let bordersLeftWidth = borders.map(border => border.widthLeft).reduce((acc, curr) => acc + curr);
+  let bordersTopWidth = borders
+    .map((border) => border.widthTop)
+    .reduce((acc, curr) => acc + curr);
+  let bordersRightWidth = borders
+    .map((border) => border.widthRight)
+    .reduce((acc, curr) => acc + curr);
+  let bordersBottomWidth = borders
+    .map((border) => border.widthBottom)
+    .reduce((acc, curr) => acc + curr);
+  let bordersLeftWidth = borders
+    .map((border) => border.widthLeft)
+    .reduce((acc, curr) => acc + curr);
 
   // calculate grid columns width
   let gridColumns = [90]; // starting value for column heads' widths
   gridColumns[0] += bordersLeftWidth * borderBaseWidth;
 
-  // sashingWidths / sashingHeights have 1 as base value for every regular width (= squares) column or regular height row 
+  // sashingWidths / sashingHeights have 1 as base value for every regular width (= squares) column or regular height row
   sashingWidths.map((width) => gridColumns.push(width * squareWidth));
 
-  let containerWidth = gridColumns.reduce((prev, curr) => prev + curr) + 'px';
-  let gridColumnsStyle = gridColumns.map(col => col + 'px').join(' ');
+  let containerWidth =
+    gridColumns.reduce((prev, curr) => prev + curr) +
+    bordersRightWidth * borderBaseWidth +
+    "px";
+  let gridColumnsStyle = gridColumns.map((col) => col + "px").join(" ");
 
   // calculate grid rows height
   let gridRows = [90]; // starting value for row heads' heights
   gridRows[0] += bordersTopWidth * borderBaseWidth;
 
   sashingHeights.map((height) => gridRows.push(height * squareWidth));
-  let containerHeight = gridRows.reduce((prev, curr) => prev + curr) + 'px';
-  let gridRowsStyle = gridRows.map(row => row + 'px').join(' ');
+  let containerHeight = gridRows.reduce((prev, curr) => prev + curr) + "px";
+  let gridRowsStyle = gridRows.map((row) => row + "px").join(" ");
 
   // build squares grid container
   const allSquaresGrid = () => {
     let styles = {
-      display: 'grid',
+      display: "grid",
       gridTemplateColumns: gridColumnsStyle,
       gridTemplateRows: gridRowsStyle,
-      width: containerWidth
-    }
+      width: containerWidth,
+    };
     return (
-      <div id="squares-container" style={styles} >
+      <div id="squares-container" style={styles}>
         {allSquares()}
       </div>
-    )
-  }
+    );
+  };
 
   // define offsets of inserted BigBlocks related to squares grid, build them
   const insertedBlocksOverlay = () => {
     let overlayBlocks = insertedBigBlocks.map((block, index) => {
-      let anchor = block.anchorSquare.split('-');
-      let widthOffset = (sashingWidths.slice(0, anchor[1]).reduce((acc, val) => acc + val, 0) * squareWidth) + bordersLeftWidth * borderBaseWidth + 90;
-      let heightOffset = (sashingHeights.slice(0, anchor[0]).reduce((acc, val) => acc + val, 0) * squareWidth) + bordersTopWidth * borderBaseWidth + 90;
+      let anchor = block.anchorSquare.split("-");
+      let widthOffset =
+        sashingWidths.slice(0, anchor[1]).reduce((acc, val) => acc + val, 0) *
+          squareWidth +
+        bordersLeftWidth * borderBaseWidth +
+        90;
+      let heightOffset =
+        sashingHeights.slice(0, anchor[0]).reduce((acc, val) => acc + val, 0) *
+          squareWidth +
+        bordersTopWidth * borderBaseWidth +
+        90;
 
       return (
         <>
-          <div className="bigblock" key={block.anchorSquare} style={{ position: "absolute", left: widthOffset + "px", top: heightOffset + "px", width: (block.stretchSquares * squareWidth) + "px", height: (block.stretchSquares * squareWidth) + "px", background: "rgba(0,0,0,0.7)" }} onClick={(event) => openBiggBlockStyler(block.anchorSquare)(event)} >
-
-            <SVGBlock anchorSquare={block.anchorSquare} rowCol={block.rowCol} blockId={block.elementBlocksId} color1={block.color1} color2={block.color2} color3={block.color3} squareWidth={squareWidth} />
-
+          <div
+            className="bigblock"
+            key={block.anchorSquare}
+            style={{
+              position: "absolute",
+              left: widthOffset + "px",
+              top: heightOffset + "px",
+              width: block.stretchSquares * squareWidth + "px",
+              height: block.stretchSquares * squareWidth + "px",
+              background: "rgba(0,0,0,0.7)",
+            }}
+            onClick={(event) => openBiggBlockStyler(block.anchorSquare)(event)}
+          >
+            <SVGBlock
+              anchorSquare={block.anchorSquare}
+              rowCol={block.rowCol}
+              blockId={block.elementBlocksId}
+              color1={block.color1}
+              color2={block.color2}
+              color3={block.color3}
+              squareWidth={squareWidth}
+            />
           </div>
         </>
-      )
-    })
-    return (
-      <>{overlayBlocks}</>
-    )
-  }
+      );
+    });
+    return <>{overlayBlocks}</>;
+  };
   const openSashColStyler = (id) => (event) => {
     event.stopPropagation();
-    openSashStyler({ rowCol: 'col', id: id });
-  }
+    openSashStyler({ rowCol: "col", id: id });
+  };
   const openSashRowStyler = (id) => (event) => {
     event.stopPropagation();
-    openSashStyler({ rowCol: 'row', id: id });
-  }
+    openSashStyler({ rowCol: "row", id: id });
+  };
   const openBiggBlockStyler = (id) => (event) => {
     event.stopPropagation();
     reopenBigBlockStyler(id);
-  }
+  };
 
   // build the grid of all squares with "column heads" and "row heads"
   const allSquares = () => {
-
     let gridItems = [];
 
     // left top corner with buttons
-    let topLeftCorner = <div className="rowhead colhead" key={0} >
-      <button className="calc-styler">
-        <span>Calculate </span>
-      </button>
+    let topLeftCorner = (
+      <div className="rowhead colhead" key={0}>
+        <button className="info-button">
+          <span>Info</span>
+        </button>
 
-      <button className="border-styler" onClick={openBorderStyler}>
-        <span>Border</span>
-      </button>
-      {
-        borderStylerIsOpen === true ?
-          <BorderStyler />
-          : null
-      }
-    </div>;
+        <button className="border-styler" onClick={openBorderStyler}>
+          <span>Border</span>
+        </button>
+        {borderStylerIsOpen === true ? <BorderStyler /> : null}
+      </div>
+    );
     gridItems.push(topLeftCorner);
 
     // first row: column heads
-    let gridColheads = cols.map((col, index) =>
+    let gridColheads = cols.map((col, index) => (
       <div className="colhead" key={`colhead-${index}`} id={`colhead-${index}`}>
         <div className="squares-settings col-sashing">
-          <button className="switch-sashing" style={{ width: sashingWidths[index] * squareWidth }} onClick={openSashColStyler(index)} >
-            <span >Sashing</span>
+          <button
+            className="switch-sashing"
+            style={{ width: sashingWidths[index] * squareWidth }}
+            onClick={openSashColStyler(index)}
+          >
+            <span>Sashing</span>
           </button>
-          {
-            sashStylerIsOpen === true && activeSashStyler.rowCol === 'col' && activeSashStyler.id === index ?
-              <SashingColStyler rowCol={'col'} id={index} />
-              : null
-          }
+          {sashStylerIsOpen === true &&
+          activeSashStyler.rowCol === "col" &&
+          activeSashStyler.id === index ? (
+            <SashingColStyler rowCol={"col"} id={index} />
+          ) : null}
         </div>
         <AddColumn colId={col} squareWidth={squareWidth} />
         <DeleteColumn colId={col} squareWidth={squareWidth} />
       </div>
-    );
+    ));
     gridItems.push(gridColheads);
 
     // each row of squares
     for (let i = 0; i < squares.length; i++) {
       // rowhead
-      let gridRowHeads =
+      let gridRowHeads = (
         <div className="rowhead" key={`rowhead-${i}`} id={`rowhead-${i}`}>
           <div className="squares-settings row-sashing">
-            <button className="switch-sashing" style={{ height: sashingHeights[i] * squareWidth }} onClick={openSashRowStyler(i)} >
-              <span >Sashing</span>
+            <button
+              className="switch-sashing"
+              style={{ height: sashingHeights[i] * squareWidth }}
+              onClick={openSashRowStyler(i)}
+            >
+              <span>Sashing</span>
             </button>
-            {
-              sashStylerIsOpen === true && activeSashStyler.rowCol === 'row' && activeSashStyler.id === i ?
-                <SashingRowStyler rowCol={'row'} id={i} />
-                : null
-            }
+            {sashStylerIsOpen === true &&
+            activeSashStyler.rowCol === "row" &&
+            activeSashStyler.id === i ? (
+              <SashingRowStyler rowCol={"row"} id={i} />
+            ) : null}
           </div>
           <AddRow rowId={i} squareWidth={squareWidth} />
           <DeleteRow rowId={i} squareWidth={squareWidth} />
         </div>
+      );
 
       gridItems.push(gridRowHeads);
 
       // squares in row
       for (let k = 0; k < squares[0].length; k++) {
-        let gridSquare =
+        let gridSquare = (
           <Square
             key={squares[i][k].id}
             id={squares[i][k].id}
@@ -177,20 +233,21 @@ const Squares = () => {
             sashingHeight={squares[i][k].sashingHeight}
             squareWidth={squareWidth}
           />
+        );
 
         gridItems.push(gridSquare);
       }
     }
     return gridItems;
-  }
+  };
 
   const bordersUnderlay = () => {
     return (
       <div className="borders-underlay">
         <Border />
       </div>
-    )
-  }
+    );
+  };
 
   return (
     <>
@@ -200,7 +257,7 @@ const Squares = () => {
         {insertedBlocksOverlay()}
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Squares;
