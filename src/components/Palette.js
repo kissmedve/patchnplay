e378defbdcec2e079049ff2fd43852e32b96bd8a
Ursula Gottschall <ursula.gottschall@webberie.de>
2021-12-01 23:05:45 +0100

    deletions and minor changes

--- src/components/Palette.js
+++ src/components/Palette.js
@@ -1,137 +1,143 @@
-import React, { useState, useContext } from "react";
-import { StylersContext } from "./StylersContext";
-import Swatches from './Swatches';
+import React from "react";
+import Swatches from "./Swatches";
 
 const Palette = ({ paletteType, squareId, rowColId, borderPos }) => {
-
-  // global states
-  const { colorSquare } = useContext(StylersContext);
-
-  // local states
-  const [colorData, setColorData] = useState({});
-
   const paletteData = [
     {
-      type: 'rect',
+      type: "rect",
       data: [
         {
-          swatchesGroup: '1',
-          swatchesTitle: 'Square',
-        }
-      ],
-    },
-    {
-      type: 'hstDown',
-      data: [
-        {
-          swatchesGroup: '1',
-          swatchesTitle: 'Left Triangle',
-        },
-        {
-          swatchesGroup: '2',
-          swatchesTitle: 'Right Triangle',
-        }
-      ]
-    },
-    {
-      type: 'hstUp',
-      data: [
-        {
-          swatchesGroup: '1',
-          swatchesTitle: 'Left Triangle',
-        },
-        {
-          swatchesGroup: '2',
-          swatchesTitle: 'Right Triangle',
-        }
-      ],
-    },
-    {
-      type: 'bigBlockCol2',
-      data: [
-        {
-          swatchesGroup: '1',
-          swatchesTitle: 'Colour 1',
-        },
-        {
-          swatchesGroup: '2',
-          swatchesTitle: 'Colour 2',
+          swatchesGroup: "1",
+          swatchesTitle: "Square",
         },
       ],
     },
     {
-      type: 'bigBlockCol3',
+      type: "hstDown",
       data: [
         {
-          swatchesGroup: '1',
-          swatchesTitle: 'Colour 1',
+          swatchesGroup: "1",
+          swatchesTitle: "Left Triangle",
         },
         {
-          swatchesGroup: '2',
-          swatchesTitle: 'Colour 2',
+          swatchesGroup: "2",
+          swatchesTitle: "Right Triangle",
+        },
+      ],
+    },
+    {
+      type: "hstUp",
+      data: [
+        {
+          swatchesGroup: "1",
+          swatchesTitle: "Left Triangle",
         },
         {
-          swatchesGroup: '3',
-          swatchesTitle: 'Colour 3',
-        }
+          swatchesGroup: "2",
+          swatchesTitle: "Right Triangle",
+        },
       ],
     },
     {
-      type: 'sashColumn',
+      type: "bigBlockCol2",
       data: [
         {
-          swatchesGroup: '1',
-          swatchesTitle: 'Sashing Column',
-        }
+          swatchesGroup: "1",
+          swatchesTitle: "Colour 1",
+        },
+        {
+          swatchesGroup: "2",
+          swatchesTitle: "Colour 2",
+        },
       ],
     },
     {
-      type: 'sashRow',
+      type: "bigBlockCol3",
       data: [
         {
-          swatchesGroup: '1',
-          swatchesTitle: 'Sashing Row',
-        }
+          swatchesGroup: "1",
+          swatchesTitle: "Colour 1",
+        },
+        {
+          swatchesGroup: "2",
+          swatchesTitle: "Colour 2",
+        },
+        {
+          swatchesGroup: "3",
+          swatchesTitle: "Colour 3",
+        },
       ],
     },
     {
-      type: 'rectSashing',
+      type: "sashColumn",
       data: [
         {
-          swatchesGroup: '1',
-          swatchesTitle: 'Sashing Cross',
-        }
+          swatchesGroup: "1",
+          swatchesTitle: "Sashing Column",
+        },
       ],
     },
     {
-      type: 'border',
+      type: "sashRow",
       data: [
         {
-          swatchesGroup: '1',
-          swatchesTitle: 'Border',
-        }
+          swatchesGroup: "1",
+          swatchesTitle: "Sashing Row",
+        },
+      ],
+    },
+    {
+      type: "rectSashing",
+      data: [
+        {
+          swatchesGroup: "1",
+          swatchesTitle: "Sashing Cross",
+        },
+      ],
+    },
+    {
+      type: "border",
+      data: [
+        {
+          swatchesGroup: "1",
+          swatchesTitle: "Border",
+        },
       ],
     },
   ];
 
-  const paletteFiltered = paletteData.filter(palette => palette.type === paletteType)[0];
+  const paletteFiltered = paletteData.filter(
+    (palette) => palette.type === paletteType
+  )[0];
   const paletteFilteredData = paletteFiltered ? paletteFiltered.data : null;
 
   return (
     <>
       <div className="color-swatches">
-
         <div className="h6">
-          {paletteFilteredData && paletteFilteredData.length > 1 ? 'Colours' : paletteFilteredData && paletteFilteredData.length === 1 ? 'Colour' : null}
+          {paletteFilteredData && paletteFilteredData.length > 1
+            ? "Colours"
+            : paletteFilteredData && paletteFilteredData.length === 1
+            ? "Colour"
+            : null}
         </div>
 
-        {paletteFilteredData && paletteFilteredData.length > 0 ? paletteFilteredData.map((item, index) => (
-          <Swatches squareId={squareId} rowColId={rowColId} paletteType={paletteType} swatchesTitle={item.swatchesTitle} swatchesGroup={item.swatchesGroup} borderPos={borderPos} key={index} />
-        )) : null}
+        {paletteFilteredData && paletteFilteredData.length > 0
+          ? paletteFilteredData.map((item, index) => (
+              <Swatches
+                squareId={squareId}
+                rowColId={rowColId}
+                paletteType={paletteType}
+                swatchesTitle={item.swatchesTitle}
+                swatchesGroup={item.swatchesGroup}
+                borderPos={borderPos}
+                key={index}
+              />
+            ))
+          : null}
       </div>
-
     </>
-  )
-}
+  );
+};
 
 export default Palette;
