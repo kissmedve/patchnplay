396adbb6e768f8981f2e10ddde36d8f641e7561d
Ursula Gottschall <ursula.gottschall@webberie.de>
2021-12-06 22:25:09 +0100

    change close function to include clicking outside

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import ReactDom from "react-dom";
 
 const Modal = ({ closeModal, modalIsOpen, children }) => {
+  const modalRef = useRef();
+
+  useEffect(() => {
+    const checkOutsideClick = (event) => {
+      // if modal is open and clicked target is not within modal, close modal
+      if (
+        modalIsOpen &&
+        modalRef.current &&
+        !modalRef.current.contains(event.target)
+      ) {
+        closeModal();
+      }
+    };
+    document.addEventListener("mousedown", checkOutsideClick);
+
+    return () => {
+      document.removeEventListener("mousedown", checkOutsideClick);
+    };
+  }, [modalIsOpen]);
 
   return ReactDom.createPortal(
     <>
       <div className={`modal ${modalIsOpen === true ? "active" : ""}`}>
-        <div className="modal-container">
+        <div ref={modalRef} className="modal-container">
           <div className="modal-header">
             <button
               className="btn btn-clear float-right"
@@ -15,15 +34,13 @@ const Modal = ({ closeModal, modalIsOpen, children }) => {
             ></button>
           </div>
           <div className="modal-body">
-            <div className="content">
-              {children}
-            </div>
+            <div className="content">{children}</div>
           </div>
         </div>
       </div>
     </>,
-    document.getElementById('modals')
-  )
+    document.getElementById("modals")
+  );
 };
 
 export default Modal;
